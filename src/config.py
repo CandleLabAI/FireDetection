4e8625107308fe603b560095a7da4c30a1582bbe
NeonVS <v.vishusaxena06072019@gmail.com>
2022-09-12 14:15:34 +0530

    Added model training and testing code

--- src/config.py
+++ src/config.py
@@ -1,7 +1,11 @@
-TRAIN_IMAGES_DIR = "../data/BALANCED_FLAME_DATASET/train"
-TEST_IMAGES_DIR = "../data/BALANCED_FLAME_DATASET/test"
+import os
+
+TRAIN_IMAGES_DIR = os.path.join(os.getcwd(), "src", "data", "BALANCED_FLAME_DATASET", "train")
+TEST_IMAGES_DIR = os.path.join(os.getcwd(), "src", "data", "BALANCED_FLAME_DATASET", "test")
+TRAINED_MODEL_SAVE_DIR = os.path.join(os.getcwd(), "src", "models", "trained_model")
 
 INITIAL_LEARNING_RATE = 0.001
-IMAGE_SIZE = (254, 254)
+IMAGE_SIZE = (254, 254, 3)
+COLOR_MODE = 'rgb'
 BATCH_SIZE = 16
-EPOCHS = 10
+EPOCHS = 1
